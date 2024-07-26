d9e74a78f441b7ea3d77b5927ccb0acddd1a6b4e
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-26 20:56:01 +0200

    copiato vecchio carosello + pushato vecchie img carosello

--- js/script.js
+++ js/script.js
@@ -1,7 +1,50 @@
-// Commento singola riga
+const imagesContainer = document.querySelector('.images-container');
+console.log('imagesContainer', imagesContainer, typeof imagesContainer);
 
-/*
+const images = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp'
+];
 
-    Commento multipla riga
+console.log(images);
+console.log(images[0]);
+console.log(images[1]);
+console.log(images[2]);
+console.log(images[3]);
+console.log(images[4]);
 
-*/
+for (let i = 0; i < images.length; i++) {
+    if (i == 0) {
+        imagesContainer.innerHTML += `<img src="${images[i]}" class="active">`
+    }
+    else {
+        imagesContainer.innerHTML += `<img src="${images[i]}">`
+    }
+}
+
+let currentlyActiveImg = 1;
+
+const nextButton = document.getElementById('next-button');
+nextButton.addEventListener('click', function () {
+    if (currentlyActiveImg < images.length) {
+        document.querySelector('.images-container > img:nth-child(' + currentlyActiveImg + ')').classList.remove('active');
+    
+        currentlyActiveImg++;
+    
+        document.querySelector('.images-container > img:nth-child(' + currentlyActiveImg + ')').classList.add('active');
+    }
+});
+
+const prevButton = document.getElementById('prev-button');
+prevButton.addEventListener('click', function () {
+    if (currentlyActiveImg > 1) {
+        document.querySelector('.images-container > img:nth-child(' + currentlyActiveImg + ')').classList.remove('active');
+    
+        currentlyActiveImg--;
+    
+        document.querySelector('.images-container > img:nth-child(' + currentlyActiveImg + ')').classList.add('active');
+    }
+});
