615bd3bb8fb86a7b5b450cab38809d6bc1c05a67
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-08-25 15:18:10 +0200

    completato esercizio aggiungendo descrizione img

--- js/script.js
+++ js/script.js
@@ -1,27 +1,53 @@
-const imagesContainer = document.querySelector('.images-container');
-console.log('imagesContainer', imagesContainer, typeof imagesContainer);
-
 const images = [
-    'img/01.webp',
-    'img/02.webp',
-    'img/03.webp',
-    'img/04.webp',
-    'img/05.webp'
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morales',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, 
+    {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, 
+    {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100-player face-off that combines looting, crafting, shootouts and chaos.",
+    }, 
+    {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, 
+    {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
 ];
 
-console.log(images);
-console.log(images[0]);
-console.log(images[1]);
-console.log(images[2]);
-console.log(images[3]);
-console.log(images[4]);
+const imagesContainer = document.querySelector('.images-container');
 
 for (let i = 0; i < images.length; i++) {
     if (i == 0) {
-        imagesContainer.innerHTML += `<img src="${images[i]}" class="active">`
+        imagesContainer.innerHTML += `
+            <div class="image-slide active">
+                <img src="${images[i].image}" class="active">
+                <div class="description">
+                    <h2>${images[i].title}</h2>
+                    <p>${images[i].text}</p>
+                </div>
+            </div>`;
     }
     else {
-        imagesContainer.innerHTML += `<img src="${images[i]}">`
+        imagesContainer.innerHTML += `
+            <div class="image-slide">
+                <img src="${images[i].image}">
+                <div class="description">
+                    <h2>${images[i].title}</h2>
+                    <p>${images[i].text}</p>
+                </div>
+            </div>`;
     }
 }
 
@@ -30,21 +56,21 @@ let currentlyActiveImg = 1;
 const nextButton = document.getElementById('next-button');
 nextButton.addEventListener('click', function () {
     if (currentlyActiveImg < images.length) {
-        document.querySelector('.images-container > img:nth-child(' + currentlyActiveImg + ')').classList.remove('active');
+        document.querySelector('.image-slide.active').classList.remove('active');
     
         currentlyActiveImg++;
     
-        document.querySelector('.images-container > img:nth-child(' + currentlyActiveImg + ')').classList.add('active');
+        document.querySelector('.image-slide:nth-child(' + currentlyActiveImg + ')').classList.add('active');
     }
 });
 
 const prevButton = document.getElementById('prev-button');
 prevButton.addEventListener('click', function () {
     if (currentlyActiveImg > 1) {
-        document.querySelector('.images-container > img:nth-child(' + currentlyActiveImg + ')').classList.remove('active');
+        document.querySelector('.image-slide.active').classList.remove('active');
     
         currentlyActiveImg--;
     
-        document.querySelector('.images-container > img:nth-child(' + currentlyActiveImg + ')').classList.add('active');
+        document.querySelector('.image-slide:nth-child(' + currentlyActiveImg + ')').classList.add('active');
     }
 });
